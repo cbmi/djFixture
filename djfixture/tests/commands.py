19e48d38fe0354eb3e20d5d4a796fd39c8c65754
dmegahan <dpm68@drexel.edu>
2013-07-26 12:17:15 -0400

    Returned commands.py to just assertAlmostEqual

--- djfixture/tests/commands.py
+++ djfixture/tests/commands.py
@@ -20,28 +20,7 @@ class FixtureTestCase(TestCase):
 		call_command('fixture','inspect',get_filename(csv_fileName1),get_filename(csv_fileName2),'mysite');
         	cmp_file = open(get_filename(cmp_fileName));
 		for line1, line2 in izip(open(get_filename('fixtures.json'),'r'),open(get_filename(cmp_fileName),'r')):
-			line1_find = line1.find(':');
-			line2_find = line2.find(':');
-
-			label_line1 = line1[:line1_find];
-			num_line1 = line1[line1_find:];
-			label_line2 = line2[:line2_find];
-			num_line2 =  line2[line2_find:];
-			
-			if num_line1:
-				try:
-					ret1 = int(num_line1);
-				except ValueError:
-					ret1 = float(num_line1);
-			
-			if num_line2:
-				try:
-					ret2 = int(num_line2);
-				except ValueError:
-					ret2 = float(num_line2);
-
-			self.assertEqual(label_line1,label_line2);
-			self.assertAlmostEqual(ret1,ret2);	
+			self.assertAlmostEqual(line1,line2);	
 
 	def test_csv_without_repeating_fields(self):
 		fileName = 'fixture_without_rep_fields';
