b92ef86bbb5a7da5b2967ac91513aca4cd525f6b
dmegahan <dpm68@drexel.edu>
2013-07-26 13:28:16 -0400

    Added try/except TypeError to commands.py

--- djfixture/tests/commands.py
+++ djfixture/tests/commands.py
@@ -8,26 +8,36 @@ from itertools import izip_longest
 
 __all__ = ('FixtureTestCase',)
 def get_filename(filename):
-	from djfixture import tests
-	return os.path.join(os.path.dirname(tests.__file__), 'fixtures', filename)
+    from djfixture import tests
+    return os.path.join(os.path.dirname(tests.__file__), 'fixtures', filename)
 
 class FixtureTestCase(TestCase):
-	def test_csv_with_repeating_fields(self):
-		fileName = 'fixture_with_rep_fields';
-       		csv_fileName1 = fileName + '.csv';
-		csv_fileName2 = fileName + '.json';
-        	cmp_fileName = 'cmp_' + fileName + '.json';
-		call_command('fixture','inspect',get_filename(csv_fileName1),get_filename(csv_fileName2),'mysite');
-        	cmp_file = open(get_filename(cmp_fileName));
-		for line1, line2 in izip(open(get_filename('fixtures.json'),'r'),open(get_filename(cmp_fileName),'r')):
-			self.assertAlmostEqual(line1,line2);	
+    def test_csv_with_repeating_fields(self):
+        fileName = 'fixture_with_rep_fields';
+            csv_fileName1 = fileName + '.csv';
+        csv_fileName2 = fileName + '.json';
+        cmp_fileName = 'cmp_' + fileName + '.json';
+        call_command('fixture','inspect',get_filename(csv_fileName1),
+                                get_filename(csv_fileName2),'mysite');
+        cmp_file = open(get_filename(cmp_fileName));
+        for line1, line2 in izip(open(get_filename('fixtures.json'),'r'),
+                                    open(get_filename(cmp_fileName),'r')):
+            try:
+                self.assertAlmostEqual(line1,line2);    
+            except TypeError:
+                self.assertEqual(line1,line2);
 
-	def test_csv_without_repeating_fields(self):
-		fileName = 'fixture_without_rep_fields';
-		csv_fileName1 = fileName + '.csv';
-		csv_fileName2 = fileName + '.json';
-		cmp_fileName = 'cmp_' + fileName + '.json';
-		call_command('fixture','inspect',get_filename(csv_fileName1),get_filename(csv_fileName2),'mysite');
-		cmp_file = open(get_filename(cmp_fileName));
-		for line1, line2 in izip(open(get_filename('fixtures.json'),'r'),open(get_filename(cmp_fileName),'r')):
-         	       self.assertAlmostEqual(line1,line2);
+    def test_csv_without_repeating_fields(self):
+        fileName = 'fixture_without_rep_fields';
+        csv_fileName1 = fileName + '.csv';
+        csv_fileName2 = fileName + '.json';
+        cmp_fileName = 'cmp_' + fileName + '.json';
+        call_command('fixture','inspect',get_filename(csv_fileName1),
+                                get_filename(csv_fileName2),'mysite');
+        cmp_file = open(get_filename(cmp_fileName));
+        for line1, line2 in izip(open(get_filename('fixtures.json'),'r'),
+                                    open(get_filename(cmp_fileName),'r')):
+            try:
+                self.assertAlmostEqual(line1,line2);
+            except TypeError:
+                self.assertEqual(line1,line2);
